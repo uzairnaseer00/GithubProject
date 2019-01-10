cbb56331cdb2bdc7555dc8b105084a27998e09f3
Uzair Naseer <uzairnaseer08@gmail.com>
2019-01-10 18:02:38 +0500

    tiny change

--- app/src/main/java/com/example/hp/githubproject/ItemDetailActivity.java
+++ app/src/main/java/com/example/hp/githubproject/ItemDetailActivity.java
@@ -29,7 +29,7 @@ public class ItemDetailActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own detail action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your own detail actionssss", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
